7ce0d5338d3fa9f394bb167989fad0d4d0a1993d
Fabio Conti <e_ternauta@live.it>
2021-12-10 18:40:44 +0100

    last build commit

---
+++ build/precache-manifest.da1b6a54694475e4b7f57b1a10867d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352a0a28826daf5221d40d27d8ec26d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53405f04176d3034a648",
+    "url": "/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "28e4e4692c60127be549",
+    "url": "/static/css/main.22170ed8.chunk.css"
+  },
+  {
+    "revision": "53405f04176d3034a648",
+    "url": "/static/js/2.c78d4042.chunk.js"
+  },
+  {
+    "revision": "6d033e71551203b8e968fa6361eb0dbf",
+    "url": "/static/js/2.c78d4042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e4e4692c60127be549",
+    "url": "/static/js/main.e3b1b52a.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
